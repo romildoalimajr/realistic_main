17e1eb84510fe6463e72d0a10408c794611607ab
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2025-04-24 12:11:46 -0300

    Implementado a Section Testimonials no HTML e o Swiper Slider no JavaScript.

--- js/app.js
+++ js/app.js
@@ -22,4 +22,31 @@ document.addEventListener("DOMContentLoaded", () => {
             activeImage.classList.add('active');
         });
     });
-})
+
+    /**
+     * Testimonials slider
+     */
+    if(document.querySelector('.swiper')){
+        var swiper = new Swiper(".swiper", {
+            loop: true,
+            effect: "fade",
+            speed: 700,
+            autoplayy: {
+                delay: 5000, 
+                disableOnInteractions: false,
+            },
+            fadeEffect: {
+                crossFade: true,
+            },
+            pagination:{
+                el: '.swiper-pagination',
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        });
+    }
+});
